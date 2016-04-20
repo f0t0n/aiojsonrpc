49d11d119bc017ba61841088ccd9ed92b49aa28c
Eugene Neydenov <t.34.oxygen@gmail.com>
2016-04-20 11:57:55 +0300

    remove ujson backoff and test; add json call to client example

--- aiojsonrpc/serializer/json.py
+++ aiojsonrpc/serializer/json.py
@@ -1,21 +1,8 @@
+import ujson as json
 from functools import partial
 from . import factory
 
 
-def import_ujson():
-    import ujson
-    return ujson
-
-
-def import_json():
-    try:
-        json = import_ujson()
-    except ImportError:
-        import json
-    return json
-
-
-json = import_json()
 loads = partial(factory.loads, json)
 dumps = partial(factory.dumps, json)
 serialize = loads
